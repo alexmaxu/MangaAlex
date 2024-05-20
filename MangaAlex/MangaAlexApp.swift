83573f0b8ded34d840d18c282ef0188899c2a093
alexmaxu <12alexmaxu@gmail.com>
2024-05-20 17:07:47 +0200

    Initial Commit

---
+++ MangaAlex/MangaAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaAlexApp.swift
+//  MangaAlex
+//
+//  Created by Alex  on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
